37fd5cfca63a7805855a33bc7119a901b7676e96
Dani <dpajaresfo@gmail.com>
2025-06-09 16:40:39 +0200

    third commit: Session, bcrypt, ejs, carpeta public

--- index.js
+++ index.js
@@ -2,11 +2,34 @@
 
 const express = require("express");
 const app = express();
+require("dotnet").config({path:".env/.env"});
+const bcrypt = require("bcrypt.js");
+const session = require("express-session"); // solo ha nivel de desarrollo
+
+//9 7 Definir o configurar la sesion
+app.use(
+    session({
+        secret:"secret", // clave para cifrar la sesion
+        resave: false, // se guarda en cada peticion
+        saveUninitialized: false, // se guarda en cada peticion cuando se produzcan cambios.
+    })
+)
+
 
 //9 3 Definimos los middlewares
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+
+//9 5 Configurar carpeta public
+
+app.use("/resource",express.static("public"));
+
+//9 6 Definir el motor de vistas
+
+app.set("view engine", "ejs");
+//app.set("views", _dirname, "/viwes"); por si la carpeta views no esta en la raiz del proyecto
+
 
 //9 4 definimos una ruta de entrada
 
