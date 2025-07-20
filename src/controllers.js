476f894ec8cccf1693a7b4f94a0a92b773ad1ddc
Dani <dpajaresfo@gmail.com>
2025-07-20 11:15:29 +0200

    añadimos la funcion de editar cursos

--- src/controllers.js
+++ src/controllers.js
@@ -1,5 +1,37 @@
 const db= require("../database/db");
-exports.save = (req, res) => {
+
+/**
+ * controlador para guardar los cursos
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.saveCursos = (req, res) => {
+    const titulo =req.body.titulo;
+    const descripcion = req.body.descripcion;
+    const visibilidad = req.body.visibilidad;
+
+    // console.log(nombre+ " "+ precio, " "+ stock);
+    db.query("INSERT INTO cursos SET ?", 
+        {
+            titulo:titulo, 
+            descripcion:descripcion, 
+            visibilidad:visibilidad
+        }, 
+        (error, results) => {
+            if (error) {
+                console.log(error);
+                res.direct("/cursos");
+                
+            }else {
+                console.log("Curso insertado correctamente");
+                res.redirect("/cursos");
+            }
+            
+        } 
+    );
+};
+
+exports.saveUser = (req, res) => {
     const nombre =req.body.nombre;
     const precio = req.body.precio;
     const stock = req.body.stock;
@@ -25,28 +57,54 @@ exports.save = (req, res) => {
     );
 };
 
-exports.update = (req, res) => {
+exports.updateCursos = (req, res) => {
     const ref = req.body.referencia;
-    const nombre = req.body.nombre;
-    const precio = req.body.precio;
-    const stock = req.body.stock;
+    const titulo = req.body.titulo;
+    const descripcion = req.body.descripcion;
+    const visibilidad = req.body.visibilidad;
 
     db.query(
-        "UPDATE productos SET ? WHERE referencia = ?",[{
-            nombre:nombre,
-            precio:precio,
-            stock:stock
+        "UPDATE cursos SET ? WHERE referencia = ?",[{
+            titulo:titulo,
+            descripcion:descripcion,
+            visibilidad:visibilidad,
         },
         ref
-        ], 
+        ],
         (error, results) => {
             if(error) {
                 console.log(error);
-                res.redirect("/admin");
+                res.redirect("/cursos");
             } else {
-                console.log("Producto actualizado correctamente");
-                res.redirect("/admin");
+                console.log("Curso actualizado correctamente");
+                res.redirect("/cursos");
             }
         }
     );
 };
+
+// exports.update = (req, res) => {
+//     const ref = req.body.referencia;
+//     const nombre = req.body.nombre;
+//     const precio = req.body.precio;
+//     const stock = req.body.stock;
+
+//     db.query(
+//         "UPDATE productos SET ? WHERE referencia = ?",[{
+//             nombre:nombre,
+//             precio:precio,
+//             stock:stock
+//         },
+//         ref
+//         ], 
+//         (error, results) => {
+//             if(error) {
+//                 console.log(error);
+//                 res.redirect("/admin");
+//             } else {
+//                 console.log("Producto actualizado correctamente");
+//                 res.redirect("/admin");
+//             }
+//         }
+//     );
+// };
--- src/router.js
+++ src/router.js
@@ -4,258 +4,306 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const db = require("../database/db");
-const {body, validation, validationResult} = require("express-validator");
+const { body, validation, validationResult } = require("express-validator");
 const crud = require("./controllers"); // para usar las operaciones CRUD
 
 //9 4 definimos una ruta de entrada (para enviar las vistas)
 
-router.get("/",(req,res) => {
-    if (req.session.loggedin){
-        res.render("index",{
-            user: req.session.name, 
-            login: true, 
-            titulo:"Home"
+router.get("/", (req, res) => {
+    if (req.session.loggedin) {
+        res.render("index", {
+            user: req.session.name,
+            login: true,
+            titulo: "Home",
         });
-    }else{
-        res.render("index",{
-            user: "Debe iniciar sessión", 
-            login: false, 
-            titulo:"Home"
+    } else {
+        res.render("index", {
+            user: "Debe iniciar sessión",
+            login: false,
+            titulo: "Home",
         });
     }
     // res.render("index",{user:"Dani"});
 });
 
-router.get("/login",(req,res) => {
-    if (req.session.loggedin){
-        res.render("login",{
-            login: true,
-        });
-    }else{
-        res.render("login",{
-            login: false,
-        });
-    }
-});
-
-router.get("/register",(req,res) => {
-    if (req.session.loggedin){ 
-        res.render("register",{
+router.get("/login", (req, res) => {
+    if (req.session.loggedin) {
+        res.render("login", {
             login: true,
         });
     } else {
-        res.render("register",{
+        res.render("login", {
             login: false,
         });
     }
 });
 
-router.get("/admin",(req,res) => {
-    // res.render("admin");
-     if (req.session.loggedin){
-         db.query("SELECT * FROM productos", (error, results) => {
-             if(error){
-                 throw error;
-             } else {
-                 // console.log(results);
-                 res.render("admin", {
-                     productos: results,
-                     login: true,
-                     rol: req.session.rol,             
-                 });
-             }
+router.get("/register", (req, res) => {
+    if (req.session.loggedin) {
+        res.render("register", {
+            login: true,
         });
-    }else{
-        res.render("admin",{
-            msg: "Acceso restringido, por favor iniciar sessión", 
-            login: false, 
-       
+    } else {
+        res.render("register", {
+            login: false,
+        });
+    }
+});
+
+router.get("/admin", (req, res) => {
+    // res.render("admin");
+    if (req.session.loggedin) {
+        db.query("SELECT * FROM usuarios", (error, results) => {
+            if (error) {
+                throw error;
+            } else {
+                // console.log(results);
+                res.render("admin", {
+                    usuarios: results,
+                    login: true,
+                    rol: req.session.rol,
+                });
+            }
+        });
+    } else {
+        res.render("admin", {
+            msg: "Acceso restringido, por favor iniciar sessión",
+            login: false,
         });
     }
     // res.render("index",{user:"Dani"});
 });
 
 router.get("/create", (req, res) => {
-    res.render("create");
-    
+    res.render("create", {
+        login: true,
+        titulo: "Crear Curso",
+    });
+});
+router.get("/createUser", (req, res) => {
+    res.render("createUser", {
+        login: true,
+        titulo: "Crear Usuario",
+    });
 });
 
-router.get("/edit/:ref", (req, res) => {
+router.get("/cursos", (req, res) => {
+    if (req.session.loggedin) {
+        db.query("SELECT * FROM cursos", (error, results) => {
+            if (error) {
+                throw error;
+            } else {
+                // console.log(results);
+                res.render("cursos", {
+                    cursos: results,
+                    login: true,
+                    rol: req.session.rol,
+                });
+            }
+        });
+    } else {
+        db.query("SELECT * FROM cursos", (error, results) => {
+            if (error) {
+                throw error;
+            } else {
+                // console.log(results);
+                res.render("cursos", {
+                    cursos: results,
+                    msg: "Acceso limitado, por favor iniciar sessión",
+                    login: false,
+                    rol: req.session.rol,
+                });
+            }
+        });
+    }
+});
+
+router.get("/editCursos/:ref", (req, res) => {
     const ref = req.params.ref;
     console.log(ref);
-    db.query("SELECT * FROM productos WHERE referencia= ?", [ref], (error, results) => {
-        if(error) {
-            throw error; 
-        }else{
-            res.render("edit", {
-                producto: results[0],
-            });
-        }
+    db.query(
+        "SELECT * FROM cursos WHERE referencia= ?",
+        [ref],
+        (error, results) => {
+            if (error) {
+                throw error;
+            } else {
+                res.render("editCursos", {
+                    curso: results[0],
+                    login: req.session.loggedin,
+                    rol: req.session.rol
+                });
+            }
         }
     );
-})
+});
 
 router.get("/delete/:ref", (req, res) => {
     const ref = req.params.ref;
-    db.query("DELETE FROM productos WHERE referencia = ?", [ref], (error, results) => {
-        if(error) {
-            throw error;
-        } else {
-            res.redirect("/admin");
+    db.query(
+        "DELETE FROM cursos WHERE referencia = ?",
+        [ref],
+        (error, results) => {
+            if (error) {
+                throw error;
+            } else {
+                res.redirect("/admin");
+            }
         }
-    }
     );
-})
+});
 
 //9 8 definimos las rutas de post
 //  definimos las rutas insert
 
-router.post("/register", [
-    body("user")
-        .isLength({ min: 4 })
-        .withMessage("El usuario debe tener 4 caracteres"),
-    body("name")
-        .isLength({ min: 4 })
-        .withMessage("El nombre debe tener 4 caracteres"),
-    body("pass")
-        .isLength({ min: 4 })
-        .withMessage("El pass debe tener 4 caracteres"),
-    body("email")
-        .isEmail()
-        .withMessage("El email no es válido"),
-    body("edad")
-        .isNumeric()
-        .withMessage("La edad debe ser un número"),
-    body("rol") // si usas rol, valida también
-        .notEmpty()
-        .withMessage("El rol es obligatorio"),
-], 
-async (req, res) => {
-    console.log("Request body:", req.body);
-    const errors = validationResult(req);
-    console.log("Validation errors:", errors.array());
-    if (!errors.isEmpty()) {
-        console.log(req.body);
-        const valores = req.body;
-        const validacionErrores = errors.array();
-        return res.render("register", {
-            validaciones: validacionErrores,
-            valores: valores,
-            titulo: "Registro",
-        });
-    }else{
-        
-        const { user, name, pass, rol } = req.body;
-        const passwordHash = await bcrypt.hash(pass, 8);
-    
-        db.query(
-            "INSERT INTO usuarios SET ?",
+router.post(
+    "/register",
+    [
+        body("user")
+            .isLength({ min: 4 })
+            .withMessage("El usuario debe tener 4 caracteres"),
+        body("name")
+            .isLength({ min: 4 })
+            .withMessage("El nombre debe tener 4 caracteres"),
+        body("pass")
+            .isLength({ min: 4 })
+            .withMessage("El pass debe tener 4 caracteres"),
+        body("rol").notEmpty(),
+        body("email")
+            .isEmail()
+            .withMessage("El email no es válido"),
+    ],
+    async (req, res) => {
+        console.log("Request body:", req.body);
+        const errors = validationResult(req);
+        console.log("Validation errors:", errors.array());
+        if (!errors.isEmpty()) 
             {
-                Usuario: user,
-                Nombre: name,
-                rol: rol,
-                pass: passwordHash,
-            },
-            (error, results) => {
-                if (error) {
-                    console.error(error);
-                    // Opcional: enviar respuesta de error
-                    return res.render("register", {
-                        alert: true,
-                        alertTitle: "Error",
-                        alertMessage: "Hubo un problema al registrar el usuario.",
-                        alertIcon: "error",
-                        titulo: "Registro",
-                    });
-                } else {
-                    res.render("register", {
-                        alert: true,
-                        alertTitle: "Register",
-                        alertMessage: "El usuario se ha registrado correctamente",
-                        alertIcon: "success",
-                        showConfirmButton: false,
-                        timer: 1500,
-                        ruta: "/",
-                        titulo: "Resgistro",
-                    });
-                }
-            }
-        );
-    }
+            console.log(req.body);
+            const valores = req.body;
+            const validacionErrores = errors.array();
+            return res.render("register", {
+                validaciones: validacionErrores,
+                valores: valores,
+                titulo: "Registro",
+            });
+        } else {
+            const { user, name, pass, rol, email } = req.body;
+            const passwordHash = await bcrypt.hash(pass, 8);
 
-});
+            db.query(
+                "INSERT INTO usuarios SET ?",
+                {
+                    Usuario: user,
+                    Nombre: name,
+                    rol: "user",
+                    pass: passwordHash,
+                    email: email,
+                },
+                (error, results) => {
+                    if (error) {
+                        console.error(error);
+                        // Opcional: enviar respuesta de error
+                        return res.render("register", {
+                            alert: true,
+                            alertTitle: "Error",
+                            alertMessage:
+                                "Hubo un problema al registrar el usuario.",
+                            alertIcon: "error",
+                            titulo: "Registro",
+                        });
+                    } else {
+                        res.render("register", {
+                            alert: true,
+                            alertTitle: "Register",
+                            alertMessage:
+                                "El usuario se ha registrado correctamente",
+                            alertIcon: "success",
+                            showConfirmButton: false,
+                            timer: 1500,
+                            ruta: "/",
+                            titulo: "Resgistro",
+                        });
+                    }
+                }
+            );
+        }
+    }
+);
 
 // definimos la ruta de login
 router.post("/auth", async (req, res) => {
     const user = req.body.user;
     const pass = req.body.pass;
 
-    if (user && pass){
+    if (user && pass) {
         db.query(
-            "SELECT * FROM usuarios WHERE usuario = ? ", 
-            [user], 
-            async(error,results) => {
-                if(
-                    results.length == 0 
-                    || !(await bcrypt.compare(pass, results[0].pass))){
-                        // res.send(
-                        //      "El usuario no existe o la contraseña es incorrecta"
-                        // );
-                        res.render("login" , {
-                            alert: true ,
-                            alertTitle: "Error" ,
-                            alertMessage: "El usuario no existe o la contraseña es incorrecta" ,
-                            alertIcon: "error" ,
-                            showConfirmButton: true ,
-                            timer: false ,
-                            ruta: "login" ,
-                            login: false,
-                            titulo: "Login",
-                        })
-                    }else{
-                        req.session.loggedin =true;
-                        req.session.name = results[0].nombre;
-                        req.session.rol = results[0].rol;
-                        // res.send("El usuario se ha logeado correctamente");
-                        res.render("login" , {
-                            alert: true,
-                            alertTitle: "Login" ,
-                            alertMessage:"El usuario se ha logeado correctamente",
-                            alertIcon:"success",
-                            showConfirmButton: false,
-                            timer:2500,
-                            ruta:"",
-                            login: true,
-                            titulo: "Login",
-                        })
-                    }
-
+            "SELECT * FROM usuarios WHERE usuario = ?",
+            [user],
+            async (error, results) => {
+                if (
+                    results.length == 0 ||
+                    !(await bcrypt.compare(pass, results[0].pass))
+                ) {
+                    // res.send(
+                    //      "El usuario no existe o la contraseña es incorrecta"
+                    // );
+                    return res.render("login", {
+                        alert: true,
+                        alertTitle: "Error",
+                        alertMessage:
+                            "El usuario no existe o la contraseña es incorrecta",
+                        alertIcon: "error",
+                        showConfirmButton: true,
+                        timer: false,
+                        ruta: "login",
+                        login: false,
+                        titulo: "Login",
+                    });
+                } else {
+                    req.session.loggedin = true;
+                    req.session.name = results[0].nombre;
+                    req.session.rol = results[0].rol;
+                    // res.send("El usuario se ha logeado correctamente");
+                    res.render("login", {
+                        alert: true,
+                        alertTitle: "Login",
+                        alertMessage: "El usuario se ha logeado correctamente",
+                        alertIcon: "success",
+                        showConfirmButton: false,
+                        timer: 2500,
+                        ruta: "",
+                        login: true,
+                        titulo: "Login",
+                    });
+                }
+            }
+        );
+    } else {
+        res.render("login", {
+            alert: true,
+            alertTitle: "Login",
+            alertMessage: "Introduzca su usuario y contraseña",
+            alertIcon: "success",
+            showConfirmButton: false,
+            timer: 2500,
+            ruta: "",
+            login: false,
+            titulo: "Login",
         });
-    }else{
-         res.render("login" , {
-                            alert: true,
-                            alertTitle: "Login" ,
-                            alertMessage:"Introduzca su usuario y contraseña",
-                            alertIcon:"success",
-                            showConfirmButton: false,
-                            timer:2500,
-                            ruta:"",
-                            login: false,
-                            titulo: "Login",
-                        })
-
     }
 });
 
 // Ruta de cierre de sesion
 
-    router.get("/logout", (req, res) => {
-        req.session.destroy(() => {
-            res.redirect("/");
-        });
+router.get("/logout", (req, res) => {
+    req.session.destroy(() => {
+        res.redirect("/");
     });
+});
 
-router.post("/save",crud.save); 
-router.post("/update",crud.update); 
+router.post("/saveCursos", crud.saveCursos);
+router.post("/saveUser", crud.saveUser); // para guardar los productos
+router.post("/updateCursos", crud.updateCursos);
 
 module.exports = router;
