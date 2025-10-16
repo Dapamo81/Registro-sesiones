8055767da84a600b7c13c3602bbc0901b90c9a99
Dani <dpajaresfo@gmail.com>
2025-10-16 18:26:53 +0200

    base de datios

--- database/db.js
+++ database/db.js
@@ -1,21 +1,19 @@
-
 const mysql = require("mysql2");
 
 const conexion = mysql.createConnection({
     host: process.env.DB_HOST,
-    user: "root",
+    user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    //port: process.env.DB_PORT //auqnue no es necesario definirlo, porque viene predefinido.
-
+    port: process.env.DB_PORT, //auqnue no es necesario definirlo, porque viene predefinido.
 });
 
 //levantamos la conexion
 
 conexion.connect((error) => {
-    if(error){
+    if (error) {
         console.log(error);
-    }else{
+    } else {
         console.log("Conectado a la Base de datos");
     }
 });
